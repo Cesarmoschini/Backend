31f1f46d0621b8550f22901c76dfbafae7914f30
CesarMoschini <cesar_moschini@hotmail.com>
2022-09-22 14:24:59 -0300

    Desafío 1

---
+++ challenge1.js
@@ -0,0 +1,41 @@
+class User {
+    name;
+    lastname;
+    pet;
+    book;
+    countPet;
+    title;
+    constructor(name,lastname, pet=[],book=[],title=[]){
+        this.name = name;
+        this.lastname = lastname;
+        this.pet = pet;
+        this.countPet = pet.length;
+        this.book = book;
+        this.title = title;
+    }
+
+    getFullName(){
+        console.log(`Su nombre completo es ${this.name} ${this.lastname}`)
+    };
+    addPet(){
+        this.pet.push()
+        console.log(this.pet)
+    };
+    numberPet(){
+        console.log(`El usuario tiene ${this.countPet} tipos de mascotas`)
+    };
+    arrayBook(){
+        console.log(this.book)
+    };
+    arrayTitleBook(){
+        console.log(this.book.map((object)=>object.title))
+    };
+}
+const user1 = new User('Juan','Rosales',['Cat','Dog'],[{title:'Cien año de soledad',author:'García Marquez'},{title:'Corazón delator',author:'Poe'}]) ;
+user1.getFullName();
+user1.addPet();
+user1.numberPet();
+user1.arrayBook();
+user1.arrayTitleBook();
+
+
