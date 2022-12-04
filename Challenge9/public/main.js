74d263418850c3f329b05f8e25e2fb571574bbc6
CesarMoschini <cesar_moschini@hotmail.com>
2022-12-04 18:38:03 -0300

    Desafío Normalización

---
+++ Challenge9/public/main.js
@@ -0,0 +1,5 @@
+import { sendMessage } from "./sendMessage.js";
+
+const formMensaje = document.getElementById('formMensajes');
+
+formMensaje.addEventListener('submit',sendMessage);
---
+++ Challenge9/public/sendMessage.js
@@ -0,0 +1,25 @@
+import { socket } from "./socketService.js";
+
+export function sendMessage (event) {
+  event.preventDefault();
+  if (email.value && mensaje.value) {
+    let data = {
+      author: {
+        email: email.value,
+        nombre: nombre.value,
+        apellido: apellido.value,
+        alias: alias.value,
+        edad: edad.value,
+      },
+      text: mensaje.value,
+    };
+    console.log('EMITIENDO SOCKET');
+
+    socket.emit('newMessage', data);
+    email.value = '';
+    nombre.value = '';
+    apellido.value = '';
+    (alias.value = ''), (edad.value = '');
+    mensaje.value = '';
+  }
+} 
