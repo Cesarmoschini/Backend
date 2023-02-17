7be9fdcbfef53a5b5713d7e63b25353186c4068d
CesarMoschini <cesar_moschini@hotmail.com>
2023-02-17 16:15:03 -0300

    Desafío 18

---
+++ Challenge18/src/ejemploGot.ts
@@ -0,0 +1,8 @@
+import { ejemploGot1 } from './ejemplos/04-got/prueba1';
+import { ejemploGot2 } from './ejemplos/04-got/prueba2';
+import { ejemploGot3 } from './ejemplos/04-got/prueba3';
+
+console.log('Ejecutando Ejemplo con modulo GOT');
+ejemploGot1();
+ejemploGot2();
+ejemploGot3();
